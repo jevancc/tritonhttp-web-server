ecece349f83331ac8ffe271bfa913f471183e8e2
Jevan Chan <jevan.cnchan@gmail.com>
2020-10-29 13:39:48 -0700

    Add ParseMIME

--- src/tritonhttp/server_utils.go
+++ src/tritonhttp/server_utils.go
@@ -1,9 +1,31 @@
 package tritonhttp
 
-/** 
-	Load and parse the mime.types file 
+import (
+	"io/ioutil"
+	"strings"
+)
+
+/**
+	Load and parse the mime.types file
 **/
 func ParseMIME(MIMEPath string) (MIMEMap map[string]string, err error) {
-	panic("todo - ParseMIME")
-}
+	MIMEMap = make(map[string]string)
 
+	fileContent, err := ioutil.ReadFile(MIMEPath)
+	if err != nil {
+		return MIMEMap, err
+	}
+
+	fileContentString := string(fileContent)
+	for _, line := range strings.Split(fileContentString, "\n") {
+		line = strings.TrimSpace(line)
+		if len(line) > 0 {
+			lineParts := strings.Split(line, " ")
+			suffix := strings.TrimSpace(lineParts[0])
+			mimeType := strings.TrimSpace(lineParts[1])
+			MIMEMap[suffix] = mimeType
+		}
+	}
+
+	return MIMEMap, err
+}
