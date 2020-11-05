4df14e7683cfc9d5a59ead5f55368a4fcae795a2
夏子翔 <zi-xiangxia@natsukoshous-MacBook-Pro.local>
2020-11-04 18:59:56 -0800

    Adding testing files

--- testing/basic.test.js
+++ testing/basic.test.js
@@ -1,29 +1,32 @@
 const { runServer } = require('./libs/server');
-const { waitForServerStart, waitForResponse, areBuffersEqual } = require('./libs/utils');
-
+const { waitForServerStart, waitForResponse, areBuffersEqual, waitForServerTimeout } = require('./libs/utils');
+function sleep(milliseconds) {
+  return new Promise((resolve) => setTimeout(() => resolve(), milliseconds));
+}
 jest.setTimeout(100000);
 
 const SERVER_PORT = 7001;
 const DOC_MAP = {
-  'index.html': Buffer.from('Hello World'),
-  'testfile.txt': Buffer.from('This is the testfile!!!'),
+  'index.html': Buffer.from('Hello World'), 
+  'testfile.txt': Buffer.from('This is the testfile!!!')
 };
-
+// copy here
 let server, createClient;
 
-beforeAll(async () => {
+beforeAll(async () => { // start server
   server = runServer({ port: SERVER_PORT, docMap: DOC_MAP });
   createClient = server.createClient;
   await waitForServerStart();
 });
-
-afterAll(() => {
+//
+afterAll(() => { // end server
   server.cleanup();
 });
 
-test('should connect to server.', async () => {
+test('should connect to server.', async () => { // each testing
   const client = createClient();
   await client.connect();
+  // await clinent - > blocking
   expect(client.ready).toBeTruthy();
 });
 
@@ -85,4 +88,77 @@ test('should respond file content with correct content length when file exists.'
   expect(response.header.code).toBe('200');
   expect(response.header.keyValues['Content-Length']).toBe(DOC_MAP['testfile.txt'].length.toString());
   expect(areBuffersEqual(response.body, DOC_MAP['testfile.txt'])).toBeTruthy();
+});
+
+test('Bad request: should respond 400 when Slash Missing.', async () => {
+  const client1 = createClient();
+  await client1.connect();
+  client1.sendHttpGet('testfile.txt', { Host: 'localhost', Connection: 'close' });
+  await waitForResponse();
+  const res1 = client1.nextHttpResponse();
+  expect(res1.header.code).toBe('400');
+  expect(res1.header.description).toBe('Bad Request');
+});
+
+test('Bad request: should respond 400 when HTTP/1.1 Missing.', async () => {
+  // HTTP version other than 1.1
+  const client2 = createClient();
+  await client2.connect();
+  client2.sendHttpGet_adj('GET', '/', { Host: 'localhost', Connection: 'close' }, 'HTTP/1.0');
+  await waitForResponse();
+  const res2 = client2.nextHttpResponse();
+  expect(res2.header.code).toBe('400');
+  expect(res2.header.description).toBe('Bad Request');
+});
+
+
+test('Bad request: should respond 400 when sending non-supported request format.', async () => {
+
+  // methods other than GET
+  const client1 = createClient();
+  await client1.connect();
+  client1.sendHttpGet_adj('POST', '/', { Host: 'localhost', Connection: 'close' }, 'HTTP/1.1');
+  await waitForResponse();
+  const header1 = client1.nextHttpResponse();
+  expect(header1.header.code).toBe('400');
+  expect(header1.header.description).toBe('Bad Request');
+  
+  
+
+
+
+});
+
+test('Bad request: should respond 400 when sending empty request', async () => {
+    // send empty format
+    const client3 = createClient();
+    await client3.connect();
+    client3.send('\r\n');
+    await waitForResponse();
+    const res3 = client3.nextHttpResponse();
+    expect(res3.header.code).toBe('400');
+    expect(res3.header.description).toBe('Bad Request');
+});
+
+
+test('Partial request + Timeout: should respond file content with correct content when file exists and close connection after timeout.', async () => {
+  const client = createClient();
+  await client.connect();
+  
+  client.sendPartialGet('/testfile.txt', { Host: 'localhost'});
+  await waitForServerTimeout();
+
+  const res1 = client.nextHttpResponse();
+  expect(res1.header.code).toBe('400');
+  
+  expect(client.isclosed).toBeTruthy();
+});
+
+test.only('Close: should respond connection header and close connection.', async () => {
+  // HTTP version other than 1.1
+  const client = createClient();
+  await client.connect();
+  client.close();
+  await client.connect();
+  expect(client.isclosed).toBeTruthy();
 });
---
+++ testing/concurr-pipe.test.js
@@ -0,0 +1,194 @@
+const { runServer } = require('./libs/server');
+const { waitForServerStart, waitForResponse, areBuffersEqual, waitForNextRequest, waitForServerTimeout } = require('./libs/utils');
+function sleep(milliseconds) {
+  return new Promise((resolve) => setTimeout(() => resolve(), milliseconds));
+}
+jest.setTimeout(100000);
+
+const SERVER_PORT = 7001;
+const DOC_MAP = {
+  'index.html': Buffer.from('Hello World'), 
+  'testfile.txt': Buffer.from('This is the testfile!!!'),
+  'long.txt': Buffer.from('This is longfile................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................'),
+  'short.txt': Buffer.from('.')
+};
+// copy here
+let server, createClient;
+
+beforeAll(async () => { // start server
+  server = runServer({ port: SERVER_PORT, docMap: DOC_MAP });
+  createClient = server.createClient;
+  await waitForServerStart();
+});
+//
+afterAll(() => { // end server
+  server.cleanup();
+});
+
+
+test('Concurrency: should respond file content for multiple clients with correct content length when file exists.', async () => {
+  const client = createClient();
+  const client1 = createClient();
+  const client2 = createClient();
+  const client3 = createClient();
+  await client.connect();
+  await client1.connect();
+  await client2.connect();
+  await client3.connect();
+
+
+  client.sendHttpGet('/testfile.txt', { Host: 'localhost', Connection: 'close' });
+  client1.sendHttpGet('/index.html', { Host: 'localhost', Connection: 'close' });
+  client2.sendHttpGet('/long.txt', { Host: 'localhost', Connection: 'close' });
+  client3.sendHttpGet('/short.txt', { Host: 'localhost', Connection: 'close' });
+  await waitForResponse();
+
+  const response = client.nextHttpResponse();
+  const res1 = client1.nextHttpResponse();
+  const res2 = client2.nextHttpResponse();
+  const res3 = client3.nextHttpResponse();
+
+  expect(response.header.keyValues['Last-Modified']).toBe(res1.header.keyValues['Last-Modified']);
+  expect(res2.header.keyValues['Last-Modified']).toBe(res3.header.keyValues['Last-Modified']);
+  expect(res1.header.keyValues['Last-Modified']).toBe(res3.header.keyValues['Last-Modified']);
+
+
+});
+
+test('bad Concurrency: should respond 400 and close connections.', async () => {
+  const client = createClient();
+  const client1 = createClient();
+  const client2 = createClient();
+  const client3 = createClient();
+  await client.connect();
+  await client1.connect();
+  await client2.connect();
+  await client3.connect();
+
+
+  client.sendHttpGet('testfile.txt', { Host: 'localhost', Connection: 'close' });
+  client1.sendHttpGet('index.html', { Host: 'localhost', Connection: 'close' });
+  client2.sendHttpGet('long.txt', { Host: 'localhost', Connection: 'close' });
+  client3.sendHttpGet('short.txt', { Host: 'localhost', Connection: 'close' });
+  await waitForResponse();
+
+  const response = client.nextHttpResponse();
+  expect(response.header.code).toBe('400');
+  expect(client.isclosed).toBeTruthy();
+
+  const res1 = client1.nextHttpResponse();
+  expect(res1.header.code).toBe('400');
+  expect(client1.isclosed).toBeTruthy();
+
+  const res2 = client2.nextHttpResponse();
+  expect(res2.header.code).toBe('400');
+  expect(client2.isclosed).toBeTruthy();
+
+  const res3 = client3.nextHttpResponse();
+  expect(res3.header.code).toBe('400');
+  expect(client3.isclosed).toBeTruthy();
+
+});
+
+
+test('Pipeline: should respond correct content in connection and closed after timeout', async () => {
+  const client = createClient();
+  await client.connect();
+  
+  client.sendHttpGet('/testfile.txt', { Host: 'localhost'});
+  await waitForResponse();
+  const res1 = client.nextHttpResponse();
+  expect(res1.header.code).toBe('200');
+  expect(res1.header.keyValues['Content-Length']).toBe(DOC_MAP['testfile.txt'].length.toString());
+  expect(areBuffersEqual(res1.body, DOC_MAP['testfile.txt'])).toBeTruthy();
+  
+  client.sendHttpGet('/index.html', { Host: 'localhost'});
+  await waitForResponse();
+  const res2 = client.nextHttpResponse();
+  expect(res2.header.code).toBe('200');
+  expect(res2.header.keyValues['Content-Length']).toBe(DOC_MAP['index.html'].length.toString());
+  expect(areBuffersEqual(res2.body, DOC_MAP['index.html'])).toBeTruthy();
+
+  client.sendHttpGet('/long.txt', { Host: 'localhost'});
+  await waitForResponse();
+  const res3 = client.nextHttpResponse();
+  expect(res3.header.code).toBe('200');
+  expect(res3.header.keyValues['Content-Length']).toBe(DOC_MAP['long.txt'].length.toString());
+  expect(areBuffersEqual(res3.body, DOC_MAP['long.txt'])).toBeTruthy();
+
+  await waitForNextRequest();
+  
+  client.sendHttpGet('/short.txt', { Host: 'localhost'});
+  await waitForResponse();
+  const res4 = client.nextHttpResponse();
+  expect(res4.header.code).toBe('200');
+  expect(res4.header.keyValues['Content-Length']).toBe(DOC_MAP['short.txt'].length.toString());
+  expect(areBuffersEqual(res4.body, DOC_MAP['short.txt'])).toBeTruthy();
+
+  client.sendHttpGet('/???.txt', { Host: 'localhost'});
+  await waitForResponse();
+  const res5 = client.nextHttpResponse();
+  expect(res5.header.code).toBe('404');
+
+  await waitForServerTimeout();
+  expect(client.isclosed).toBeTruthy();
+  
+});
+
+test('Pipeline: should respond correct content in connection with 404 request', async () => {
+  const client = createClient();
+  await client.connect();
+  
+  client.sendHttpGet('/testfile.txt', { Host: 'localhost'});
+  client.sendHttpGet('/index.html', { Host: 'localhost'});
+  client.sendHttpGet('/long.txt', { Host: 'localhost'});
+  client.sendHttpGet('/short.txt', { Host: 'localhost'});
+  client.sendHttpGet('/???.txt', { Host: 'localhost'});
+  await waitForResponse();
+
+  const res1 = client.nextHttpResponse();
+  expect(res1.header.code).toBe('200');
+  expect(res1.header.keyValues['Content-Length']).toBe(DOC_MAP['testfile.txt'].length.toString());
+  expect(areBuffersEqual(res1.body, DOC_MAP['testfile.txt'])).toBeTruthy();
+  
+  
+  const res2 = client.nextHttpResponse();
+  expect(res2.header.code).toBe('200');
+  expect(res2.header.keyValues['Content-Length']).toBe(DOC_MAP['index.html'].length.toString());
+  expect(areBuffersEqual(res2.body, DOC_MAP['index.html'])).toBeTruthy();
+
+  
+  const res3 = client.nextHttpResponse();
+  expect(res3.header.code).toBe('200');
+  expect(res3.header.keyValues['Content-Length']).toBe(DOC_MAP['long.txt'].length.toString());
+  expect(areBuffersEqual(res3.body, DOC_MAP['long.txt'])).toBeTruthy();
+
+  const res4 = client.nextHttpResponse();
+  expect(res4.header.code).toBe('200');
+  expect(res4.header.keyValues['Content-Length']).toBe(DOC_MAP['short.txt'].length.toString());
+  expect(areBuffersEqual(res4.body, DOC_MAP['short.txt'])).toBeTruthy();
+
+  const res5 = client.nextHttpResponse();
+  expect(res5.header.code).toBe('404');
+
+});
+
+test('Pipeline: where second request is malformed ', async () => {
+  const client = createClient();
+  await client.connect();
+  
+  client.sendHttpGet('/testfile.txt', { Host: 'localhost'});
+  await waitForResponse();
+  const res1 = client.nextHttpResponse();
+  expect(res1.header.code).toBe('200');
+  expect(res1.header.keyValues['Content-Length']).toBe(DOC_MAP['testfile.txt'].length.toString());
+  expect(areBuffersEqual(res1.body, DOC_MAP['testfile.txt'])).toBeTruthy();
+  
+  client.sendHttpGet('index.html', { Host: 'localhost'});
+  await waitForResponse();
+  const res2 = client.nextHttpResponse();
+  expect(res2.header.code).toBe('400');
+
+  expect(client.isclosed).toBeTruthy();
+  
+});
